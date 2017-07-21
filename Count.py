76124bd0798e242ac7d39e4843e63d89a07c8946
Sanyam Gupta <sanyam.gupta0802@gmail.com>
2017-07-21 15:57:18 +0800

    Add files via upload

--- Count.py
+++ Count.py
@@ -1,28 +1,29 @@
 import pandas as pd
-csvin = pd.read_csv('Sorted.csv', nrows = 17)
-prev_val = 0
 
-count = [[0 for i in range(4)] for j in range(24)]
+for x in range(504367):
+	csvin = pd.read_csv('complete.csv', skiprows = x*17 + 1, nrows = 16)
+	prev_val = 0
 
+	count = [[0 for i in range(4)] for j in range(24)]
 
-for j in range(18,42):
-	row_iterator = csvin.iterrows()
-	_, last = row_iterator.__next__()
+	for j in range(18,42):
+		row_iterator = csvin.iterrows()
+		_, last = row_iterator.__next__()
 
-	for index, row in row_iterator: 
-		prev_val = float(last[j])
-		data = float(row[j]) - prev_val
+		for index, row in row_iterator: 
+			prev_val = float(last[j])
+			data = float(row[j]) - prev_val
 
-		if (data == 0):
-			if (prev_val == 1):
-				count[j-18][2] += 1
-			else:
-				count[j-18][0] += 1  
-		elif (data == 1):
-			count[j-18][1] += 1
-		elif (data == -1):
-			count[j-18][3] += 1
+			if (data == 0):
+				if (prev_val == 1):
+					count[j-18][2] += 1
+				else:
+					count[j-18][0] += 1 
+			elif (data == 1):
+				count[j-18][1] += 1
+			elif (data == -1):
+				count[j-18][3] += 1
 		
-		last = row
+			last = row
 
-print (count)
+	print (count)
