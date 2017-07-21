7bb6fac8f7643d71d95fe2879f06d8a09eca34f9
Tallis Jeng <alog1024@gmail.com>
2017-07-21 14:16:45 +0800

    tidy up data

--- csv_reader.py
+++ csv_reader.py
@@ -10,21 +10,32 @@ python3 csv_reader.py csv_file_name.csv
 
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 '''
+
 import sys
 import csv
 
+'''
+read csv file
+print to command line output
+'''
 def read_dt(data):
 	with open(data, 'r', newline='') as rf:
 		record_cnt = 0
 		reader = csv.reader(rf, delimiter=",", quotechar='|') # csv_reader
 
 		for row in reader:
+			#if record_cnt < 2: print("Record # %d" %record_cnt)
 			print("Record # %d" %record_cnt)
 			field_cnt = 0
 			for field in row:
-				print("\t Field # %d \t %s" %(field_cnt,field))
+				#if record_cnt < 2: print("\t Field # %d \t %s" %(field_cnt,field))
+				#print("\t Field # %d \t %s" %(field_cnt,field))
 				field_cnt += 1
 			record_cnt += 1
+		print("# of Record %d" %record_cnt)
 
+
+# read file from command line argument
+# argv[0] is this file name (csv_reader.py)
 file = sys.argv[1]
 read_dt(file) 
--- dt_tidy.py
+++ dt_tidy.py
@@ -3,24 +3,29 @@
 dt_tidy.py
 
 Prepare input data for DNN
-0. Remove header
-1. Remove cusId
-2. merge 1stContract and FetchDate by introducing cusTime = (1stContract - FetchDate)
+1. Remove CusId
+2. merge FetchDate and 1stContract by introducing CusTime = (FetchDate - 1stContract )
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ''' 
 
 import csv
 
 def  tidy_dt(data):
-	with open(data, 'r', newline='') as rf, open("sort.csv", 'w', newline='') as wf:
+	with open(data, 'r', newline='') as rf, open("data.csv", 'w', newline='') as wf:
 
 		reader = csv.reader(rf, delimiter=",", quotechar='|') # csv_reader
 		writer = csv.writer(wf, delimiter=",", quotechar='|') # csv_writer
 
-		fields = next(reader, None) # read headers
-		writer.writerow(fields) # write headers
+		fields = next(reader, None)	# read headers
+		fields[0] = "CusTime"
+		del fields[6]	# remove 1stContract
+		del fields[1]	# remove CusId
+		writer.writerow(fields)	# write headers
 
-		for row in reader:
+		for row in reader: 
+			row[0] = int(row[0]) - int(row[6])	# CusTime = (FetchDate - 1stContract )
+			del row[6]	# remove 1stContract
+			del row[1]	# remove CusId
 			writer.writerow(row)
 
-sort_dt("num.csv")
+tidy_dt("features.csv")
